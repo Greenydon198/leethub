b44bc73301c633f5086be1e5f38390c268b9a368
Greenydon198 <117624471+Greenydon198@users.noreply.github.com>
2022-11-22 22:35:11 +0530

    Time: 10 ms (55.89%), Space: 43.9 MB (49.95%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countKDifference(int[] a, int k) {
+        int c=0;
+        Arrays.sort(a);
+        // int t =-100000000;
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++)
+                if(a[j]-a[i]==k){
+                    c++;
+                    // break;
+                }
+            else if(a[j]-a[i]>k)
+                break;
+            // t=a[i];
+        }
+        return c;
+    }
+}
