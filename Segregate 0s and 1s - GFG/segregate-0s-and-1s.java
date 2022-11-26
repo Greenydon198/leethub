fa2e6396d900f0e04131e82b61c8a38a9ae71eb1
Greenydon198 <117624471+Greenydon198@users.noreply.github.com>
2022-11-26 14:07:33 +0530

    Added solution - LeetHub

---
+++ Segregate 0s and 1s - GFG/segregate-0s-and-1s.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            new Solution().segregate0and1(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void segregate0and1(int[] a, int n) {
+        // code here
+        // Arrays.sort(a);
+        int s=0,e=n-1;
+        while(s<e){
+            if(a[s]==0)
+                s++;
+            if(a[e]==1)
+                e--;
+            if(s<e && a[s]==1 && a[e]==0){
+                a[s++]=0;
+                a[e--]=1;
+            }
+        }
+    }
+}
