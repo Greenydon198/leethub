16a34ecac107b68d55db7439383f94960b983686
Greenydon198 <117624471+Greenydon198@users.noreply.github.com>
2022-12-27 15:57:30 +0530

    Added solution - LeetHub

---
+++ Burning Tree - GFG/burning-tree.java
@@ -0,0 +1,160 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
+
+class GfG {
+
+	static Node buildTree(String str) {
+
+		if (str.length() == 0 || str.charAt(0) == 'N') {
+			return null;
+		}
+
+		String ip[] = str.split(" ");
+		// Create the root of the tree
+		Node root = new Node(Integer.parseInt(ip[0]));
+		// Push the root to the queue
+
+		Queue<Node> queue = new LinkedList<>();
+
+		queue.add(root);
+		// Starting from the second element
+
+		int i = 1;
+		while (queue.size() > 0 && i < ip.length) {
+
+			// Get and remove the front of the queue
+			Node currNode = queue.peek();
+			queue.remove();
+
+			// Get the current node's value from the string
+			String currVal = ip[i];
+
+			// If the left child is not null
+			if (!currVal.equals("N")) {
+
+				// Create the left child for the current node
+				currNode.left = new Node(Integer.parseInt(currVal));
+				// Push it to the queue
+				queue.add(currNode.left);
+			}
+
+			// For the right child
+			i++;
+			if (i >= ip.length)
+				break;
+
+			currVal = ip[i];
+
+			// If the right child is not null
+			if (!currVal.equals("N")) {
+
+				// Create the right child for the current node
+				currNode.right = new Node(Integer.parseInt(currVal));
+
+				// Push it to the queue
+				queue.add(currNode.right);
+			}
+			i++;
+		}
+
+		return root;
+	}
+
+	static void printInorder(Node root) {
+		if (root == null)
+			return;
+
+		printInorder(root.left);
+		System.out.print(root.data + " ");
+
+		printInorder(root.right);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		while (t > 0) {
+			String s = br.readLine();
+			int target = Integer.parseInt(br.readLine());
+			Node root = buildTree(s);
+
+			Solution g = new Solution();
+			System.out.println(g.minTime(root, target));
+			t--;
+
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    /*class Node {
+    	int data;
+    	Node left;
+    	Node right;
+    
+    	Node(int data) {
+    		this.data = data;
+    		left = null;
+    		right = null;
+    	}
+    }*/
+    public static boolean path(Node root, int k,ArrayList<Node> path ) {
+        if(root==null)return false;
+        if(root.data==k){
+            path.add(root);
+            return true;
+        }
+        boolean res = path(root.left,k,path) || path(root.right,k,path);
+        if(res)path.add(root);
+        return res;
+    }
+    public static int minTime(Node root, int k) 
+    {
+        // Your code goes here
+        ArrayList<Node> path = new ArrayList<>();
+        path(root,k,path);
+        // System.out.println(path);
+        Node block = null;
+        int max = 0;
+        for(int i=0;i<path.size();i++){
+            int temp = down(path.get(i),0,block);
+            max = Math.max(max,i+temp);
+            // System.out.println(max +" "+temp+" "+path.get(i).data);
+            block = path.get(i);
+        }
+        return max-1;
+    }
+    public static int down(Node root,int d,Node block){
+        if(root==null)return 0;
+        if(root==block)return 0;
+        return 1+Math.max(down(root.left,d+1,block),down(root.right,d+1,block));
+    }
+}
