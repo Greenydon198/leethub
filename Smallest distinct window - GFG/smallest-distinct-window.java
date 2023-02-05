2b299422f824780b69a9a1cfa37022583ade7389
Greenydon198 <117624471+Greenydon198@users.noreply.github.com>
2023-02-05 16:35:08 +0530

    Added solution - LeetHub

--- Smallest distinct window - GFG/smallest-distinct-window.java
+++ Smallest distinct window - GFG/smallest-distinct-window.java
@@ -30,30 +30,44 @@ class GFG {
 class Solution {
     public int findSubString( String s) {
         // Your code goes here
+        int freq[] = new int[256];//store distinct elements
+        int dist = 0;//cnt the no.of unique elements
         int n = s.length();
-        int count = charcount(s,n);
-        int ans = n+1;
-         HashMap<Character,Integer> hm = new HashMap<>();
-        //  System.out.println(count);
-        for(int i=0,j=0;i<n;i++){
+        for(int i=0;i<n;i++){
             char temp = s.charAt(i);
+            freq[temp]++;
+            if(freq[temp]==1)dist++;
+        }
+        
+        int st = 0;
+        int en =0;
+        int ans = Integer.MAX_VALUE;//to get min len of all unique elements
+        
+        HashMap<Character,Integer> hm = new HashMap<>();//to store variables and their cnt
+        
+        while(en<n){
+            char temp = s.charAt(en);
             hm.put(temp,hm.getOrDefault(temp,0)+1);
-            while(hm.size()==count){
-                char t = s.charAt(j);
-                ans = Math.min(ans,i-j+1);
-                if(hm.get(t)==1)hm.remove(t);
-                else
-                    hm.put(t,hm.get(t)-1);
-                j++;
+            if(hm.get(temp)==1)dist--;
+            en++;
+            
+            while(dist<=0){
+                ans = Math.min(en-st,ans);//updates ans everytime length is minimum
+                
+                char start = s.charAt(st);
+                hm.put(start,hm.get(start)-1);
+                
+                if(hm.get(start)==0)
+                    dist++;
+                st++;
             }
         }
         return ans;
     }
-    int charcount(String s,int n){
-        HashMap<Character,Integer> hm = new HashMap<>();
-        for(int i=0;i<n;i++){
-            hm.put(s.charAt(i),1);
-        }
-        return hm.size();
-    }
 }
+
+
+
+
+
+
